07fb08493a266594859e669b569a7ab0c069b5ed
Felipe Cornejo <felicornejo@gmail.com>
2024-06-18 12:05:54 -0300

    chore: remove unused variables

--- modules/iovation/index.ts
+++ modules/iovation/index.ts
@@ -1,26 +1,5 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to Iovation.web.ts
-// and on native platforms to Iovation.ts
-import IovationModule from './src/IovationModule';
-import IovationView from './src/IovationView';
-import { ChangeEventPayload, IovationViewProps } from './src/Iovation.types';
-
-// Get the native constant value.
-export const PI = IovationModule.PI;
+import IovationModule from "./src/IovationModule";
 
 export function hello(): string {
   return IovationModule.hello();
 }
-
-export async function setValueAsync(value: string) {
-  return await IovationModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(IovationModule ?? NativeModulesProxy.Iovation);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { IovationView, IovationViewProps, ChangeEventPayload };
